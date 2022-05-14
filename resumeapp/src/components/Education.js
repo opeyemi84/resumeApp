1c1411807edb52e46389362636a801b2392819d7
Opeyemi <opeadetola@gmail.com>
2022-05-14 01:18:05 -0400

    Modified file for onChange event handlers

--- resumeapp/src/components/Education.js
+++ resumeapp/src/components/Education.js
@@ -1,28 +1,105 @@
 import React, { Component } from "react";
 
 class Education extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      school: "",
+      degree: "",
+      majors: "",
+      from: "",
+      to: "",
+      cgpa: "",
+    };
+  }
+
+  onSchoolChange = (event) => {
+    this.setState({
+      school: event.target.value,
+    });
+  };
+
+  onDegreeChange = (event) => {
+    this.setState({
+      degree: event.target.value,
+    });
+  };
+
+  onMajorsChange = (event) => {
+    this.setState({
+      majors: event.target.value,
+    });
+  };
+
+  onFromChange = (event) => {
+    this.setState({
+      from: event.target.value,
+    });
+  };
+
+  onToChange = (event) => {
+    this.setState({
+      to: event.target.value,
+    });
+  };
+
+  oncgpaChange = (event) => {
+    this.setState({
+      cgpa: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>Education</h2>
-        <label>School:</label>
-        <input type="text" value=""></input>
-        <br></br>
-        <label>Degree Obtained:</label>
-        <input type="text" value=""></input>
-        <br></br>
-        <label>Majors:</label>
-        <input type="text" value=""></input>
-        <br></br>
-        <label>From:</label>
-        <input type="text" value=""></input>
-        <br></br>
-        <label>To:</label>
-        <input type="text" value=""></input>
-        <br></br>
-        <label>CPGA:</label>
-        <input type="text" value=""></input>
-        <br></br>
+        <div className="section-header">
+          <h2>Education</h2>
+        </div>
+        <div className="form-control">
+          <label>School:</label>
+          <input
+            type="text"
+            value={this.state.school}
+            onChange={this.onSchoolChange}
+          ></input>
+          <br></br>
+          <label>Degree Obtained:</label>
+          <input
+            type="text"
+            value={this.state.degree}
+            onChange={this.onDegreeChange}
+          ></input>
+          <br></br>
+          <label>Majors:</label>
+          <input
+            type="text"
+            value={this.state.majors}
+            onChange={this.onMajorsChange}
+          ></input>
+          <br></br>
+          <label>From:</label>
+          <input
+            type="text"
+            value={this.state.from}
+            onChange={this.onFromChange}
+          ></input>
+          <br></br>
+          <label>To:</label>
+          <input
+            type="text"
+            value={this.state.to}
+            onChange={this.onToChange}
+          ></input>
+          <br></br>
+          <label>CGPA:</label>
+          <input
+            type="text"
+            value={this.state.cgpa}
+            onChange={this.oncgpaChange}
+          ></input>
+          <br></br>
+        </div>
       </div>
     );
   }
