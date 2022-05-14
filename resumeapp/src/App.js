72ba61c48e14adb96a9931444e8dcdc553c5e5d5
Opeyemi <opeadetola@gmail.com>
2022-05-13 20:04:37 -0400

    Added components to app.js comp

--- resumeapp/src/App.js
+++ resumeapp/src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div>
-      <h1>Resume Builder App</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import GenInformation from "./components/GenInformation";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Resume Builder</h1>
+        <GenInformation />
+        <Education />
+        <Experience />
+      </div>
+    );
+  }
 }
 
 export default App;
