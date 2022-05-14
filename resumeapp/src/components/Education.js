da3bb144baf9edade712171bbe5b970a4db8d1e8
Opeyemi <opeadetola@gmail.com>
2022-05-13 20:08:31 -0400

    Added Education.js component

---
+++ resumeapp/src/components/Education.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Education extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Education</h2>
+        <label>School:</label>
+        <input type="text" value=""></input>
+        <br></br>
+        <label>Degree Obtained:</label>
+        <input type="text" value=""></input>
+        <br></br>
+        <label>Majors:</label>
+        <input type="text" value=""></input>
+        <br></br>
+        <label>From:</label>
+        <input type="text" value=""></input>
+        <br></br>
+        <label>To:</label>
+        <input type="text" value=""></input>
+        <br></br>
+        <label>CPGA:</label>
+        <input type="text" value=""></input>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+export default Education;
